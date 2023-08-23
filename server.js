51def95cb8e29565c01d116f8376cc956fb50b0a
unknown <kingwebbe@gmail.com>
2023-08-23 18:30:02 -0400

    got server running added api to target homepage, login, sign up

--- server.js
+++ server.js
@@ -1,39 +1,41 @@
 const path = require('path');
 const express = require('express');
+const app = express();
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const compression = require('compression');
+const helpers = require('./utils/auth');
+// const compression = require('compression');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-// const hbs = exphbs.create({  });
+ const hbs = exphbs.create({ helpers });
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 // app.use(compression());
-// app.use(session(sess));
+app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+ app.set('view engine', 'handlebars');
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+const routes = require('./controllers');
+ app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
