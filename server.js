cd085907b02cf5f350fbbbb0e000d55e11a69b09
Youssef <youssefkaroui6@gmail.comm>
2023-08-25 12:13:01 -0400

    worked on login.js and logout.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const helpers = require('./utils/auth');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
@@ -16,7 +16,11 @@ const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
