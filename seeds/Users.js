6456ed36d38b7a5579d4659def7450257292fd3d
saron <133171838+saronkal@users.noreply.github.com>
2023-08-28 18:29:39 -0400

    Update Users.js

--- seeds/Users.js
+++ seeds/Users.js
@@ -1,1 +1,21 @@
-
+const{User}=require('./models');
+const UserData = 
+[
+    {
+      "name": "Sal",
+      "email": "sal@hotmail.com",
+      "password": "password12345"
+    },
+    {
+      "name": "Lernantino",
+      "email": "lernantino@gmail.com",
+      "password": "password12345"
+    },
+    {
+      "name": "Amiko",
+      "email": "amiko2k20@aol.com",
+      "password": "password12345"
+    }
+  ]
+  
+  
