136c912f48c479f56bf38ca6e4b39858f25e57bc
Youssef <youssefkaroui6@gmail.comm>
2023-08-30 21:56:38 -0400

    updated models,controller routes; fixed pathing issues for home pages

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models/User.js');
+const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
   console.log("Incoming Data: ", req.body)
@@ -11,7 +11,7 @@ router.post('/', async (req, res) => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
 
-      res.status(200).json(userData);
+      // res.status(200).json(userData);
       res.redirect('/login') // OR MAYBE --> Redirect to an AUtorized endpoint(?)
     });
   } catch (err) {
